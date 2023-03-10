bf062610cd867d3e5256d74d8feebe667c85cca8
TheRjDev <ramsus85@protonmail.com>
2023-03-09 22:21:52 -0500

    updated

--- bot.py
+++ bot.py
@@ -3,19 +3,9 @@
 # pylint: disable=unused-argument, wrong-import-position
 
 # This program is dedicated to the public domain under the CC0 license.
-
-
 """
 
 Simple Bot to send ip of personal server.
-
-
-Usage:
-
-Press Ctrl-C on the command line or send a signal to the process to stop the
-
-bot.
-
 
 Note:
 
@@ -24,7 +14,7 @@ To use arbitrary callback data, you must install ptb via
 `pip install python-telegram-bot[callback-data]`
 
 """
-
+import asyncio
 
 import logging
 
@@ -39,8 +29,8 @@ import datetime as dt
 # Enable logging
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO)
 
 
 # Define a few command handlers. These usually take the two arguments update and
@@ -54,38 +44,58 @@ logging.basicConfig(
 # This being an example and not having context present confusing beginners,
 
 # we decided to have it present as context.
-
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-
     """Sends explanation on how to use the bot."""
-    await update.message.reply_text(
-        "Hi! this bot sends you the ip of the server using /send."
-    )
+    await update.message.reply_html(
+        "Hi! this bot sends you the ip of the server using <em>/send</em>.")
 
 
-async def send_ip(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+def ip_change() -> bool:
+    """Sends the ip of the server."""
+    with open("ip.txt", "r") as ip_file:
+        ip = str(ip_file.read())
+        os.system(
+            "dig @resolver1.opendns.com myip.opendns.com +short > ip.txt")
+        new_ip = str(ip_file.read())
+    return ip != new_ip
 
-    """Send the ip of the server"""
+
+def ip() -> str:
+    """Sends the ip of the server."""
     os.system("dig @resolver1.opendns.com myip.opendns.com +short > ip.txt")
     with open("ip.txt", "r") as ip_file:
         ip = str(ip_file.read())
+    return ip
+
+
+async def send_changed_ip(update: Update,
+                          context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send the ip of the server"""
+    while True:
+        if ip_change():
+            ip = ip()
+            await update.message.reply_text(ip)
+            await asyncio.sleep(3600)
+
+
+async def send_ip(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send the ip of the server"""
+    ip = ip()
     await update.message.reply_text(ip)
 
 
 def main() -> None:
-
     """Run bot."""
 
     # Create the Application and pass it your bot's token.
     with open("token.txt", "r") as token:
         tok = str(token.read())
 
-    application = (
-        Application.builder()
-        .token()
-        .build()
-    )
+    application = (Application.builder().token(tok).build())
+
+    # Verify if ip has changed every hour and send it if it has
+
+    application.add_handler(CommandHandler("send_changed", send_changed_ip))
 
     # on different commands - answer in Telegram
 
@@ -95,7 +105,7 @@ def main() -> None:
 
     application.run_polling()
 
+
 if __name__ == "__main__":
 
     main()
-
